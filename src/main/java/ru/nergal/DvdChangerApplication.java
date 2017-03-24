6a14214da3abaf0f54b10fd391c6a46841a317d3
nergal-perm <nergal@psu.ru>
2017-03-24 15:35:14 +0500

    Springboot boilerplate code

---
+++ src/main/java/ru/nergal/DvdChangerApplication.java
@@ -0,0 +1,12 @@
+package ru.nergal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdChangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvdChangerApplication.class, args);
+	}
+}
